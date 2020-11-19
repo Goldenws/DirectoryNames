ce10e194cd51a2edae2207e47a09a9a569e4b925
RolandStrod <KhaleAldeAsad@gmail.com>
2020-11-19 13:09:12 +0200

    CreateFile

---
+++ ClearFolder/DeleteFile/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+namespace DeleteFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileDelete();
+        }
+
+        public static void FileDelete()
+        {
+            string rootPath = @"C:\Users\Roland Strod\Samples\playerTwo";
+            string fileName = "ball.txt";
+
+            if(File.Exists(Path.Combine(rootPath, fileName)))
+            {
+                File.Delete(Path.Combine(rootPath, fileName));
+            }
+            else
+            {
+                Console.WriteLine("File not found");
+            }
+        }
+
+    }
+}
---
+++ ClearFolder/MoveFiles/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+namespace MoveFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MoveFile();
+            Console.WriteLine("Job Done!");
+        }
+
+        public static void MoveFile()
+        {
+            string sourceDirectory = @"C:\Users\Roland Strod\Samples\playerOne";
+            string destinationPath = @"C:\Users\Roland Strod\Samples\playerTwo";
+            string fileName = "ball.txt";
+
+            File.Move(Path.Combine(sourceDirectory, fileName), Path.Combine(destinationPath, fileName));
+        }
+    }
+}
